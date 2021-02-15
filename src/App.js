e42838f6e226c29ed34f76f17e2f5a582905d19b
Thanapat <t.veerakajorn@gmail.com>
2021-02-15 14:57:22 -0500

    commit tree a05a5a4808c1c083de22e094be370224f5ccaa18

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React from 'react';
+
+class App extends React.Component {
+    state = { inSpaceNow: [] }
+
+    componentDidMount() {
+        return fetch('http://api.open-notify.org/astros.json')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    inSpaceNow: data.people
+                })
+            })
+        }
+
+    render() {
+        const peopleInSpace = this.state.inSpaceNow.map((astronaut, idx) => {
+        return <div key={idx}>Astronaut: {astronaut.name} - Space Craft: {astronaut.craft}</div>
+        })
+        return (
+            <div>
+                {peopleInSpace}
+            </div>
+        )
+    }
+}
+
+export default App; 
